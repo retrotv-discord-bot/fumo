c5b1e91e4b04b586d8cd2b8322b4b430212da4df
yjj8353 <yjj8353@gmail.com>
2025-04-08 14:35:45 +0900

    임베드 내에서 이미지의 url을 이용해 이미지가 랜덤 선택되는 예제 추가

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-
 import random
 import os
 from dotenv import load_dotenv
@@ -18,11 +17,37 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'{bot.user} 봇이 시작되었습니다!')
 
-@bot.command(name='동전')
-async def coin(ctx):
-    # 0 또는 1을 랜덤으로 선택
-    result = random.choice(['앞면', '뒷면'])
-    await ctx.send(f'동전을 던진 결과: {result}')
+@bot.command(name='후모')
+async def fumo(ctx):
+    # 웹호스팅된 이미지 URL 목록
+    # 여기에 이미지 URL을 추가하세요. 예시:
+    # image_urls = [
+    #     "https://example.com/image1.jpg",
+    #     "https://example.com/image2.png",
+    #     "https://example.com/image3.gif"
+    # ]
+    image_urls = [
+        'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdL0THw%2FbtrWcLGFH0C%2F7Uo1urDfHWVe0Dnx7c0vlk%2Fimg.png',
+        'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FqCrHm%2FbtrWefAdT3o%2F30MGokk5KdhUZnz33C28fk%2Fimg.png'
+    ]
+    
+    if not image_urls:
+        await ctx.send('이미지 URL이 설정되지 않았습니다! bot.py 파일의 image_urls 리스트에 이미지 URL을 추가해주세요.')
+        return
+    
+    # 랜덤으로 이미지 URL 선택
+    random_image_url = random.choice(image_urls)
+    
+    # 임베드 생성
+    embed = discord.Embed(
+        title="후모 이미지",
+        description="랜덤으로 선택된 후모 이미지입니다!",
+        color=discord.Color.blue()
+    )
+    
+    # 임베드에 이미지 URL 설정
+    embed.set_image(url=random_image_url)
+    await ctx.send(embed=embed)
 
 # 봇 실행
 bot.run(TOKEN)
