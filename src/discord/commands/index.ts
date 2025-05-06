9b42df0c5c965ed5c837d50396597652a92b4643
yjj8353 <yjj8353@gmail.com>
2025-05-07 00:30:57 +0900

    ts-node 경로 수정

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -1,7 +1,6 @@
 import ContextMenuCommand from "../../templates/context-menu-command";
 import PrefixCommand from "../../templates/prefix-command";
 import SlashCommand from "../../templates/slash-command";
-import contextUserinfo from "./context-menu/userinfo";
 import slashPing from "./slash/ping";
 import prefixPing from "./prefix/ping";
 
@@ -9,6 +8,6 @@ import prefixPing from "./prefix/ping";
  * 사용하는 컨텍스트 메뉴, 슬래시 명령어, 접두 명령어를 선언함. 선언된 명령어는 index.ts에서 호출됨.
  * Declare the context menu, slash commands, and prefix commands to be used. The declared commands are called in index.ts.
  */
-export const contextMenuCommands: ContextMenuCommand[] = [contextUserinfo];
+export const contextMenuCommands: ContextMenuCommand[] = [];
 export const slashCommands: SlashCommand[] = [slashPing];
 export const prefixCommands: PrefixCommand[] = [prefixPing];
